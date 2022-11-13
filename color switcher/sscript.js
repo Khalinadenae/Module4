a48387f2e9894243eb082ccbbf515b22682c5f2c
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-11-13 06:51:16 -0800

    comment

--- color switcher/sscript.js
+++ color switcher/sscript.js
@@ -1,4 +1,8 @@
+//selectedOptions contains a list of the <option> elements contained within the <select> element that are currently selected.
+// selectedIndex property sets or returns the index of the selected option in a drop-down list
+
 let dropdownList = document.getElementById('selections');
+
 
     dropdownList.onchange = function changeBgColor(event) {
       let selecetedIndex = dropdownList.selectedIndex;
@@ -6,3 +10,4 @@ let dropdownList = document.getElementById('selections');
     document.body.style.backgroundColor = selectedOption.text;
    }
     
+
